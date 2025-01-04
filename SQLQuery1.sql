d8fa3a2971ed57ac74299caeef5cd4afc6a533c8
SatyamAshish06 <satyamashish399@gmail.com>
2025-01-04 10:19:43 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,24 @@
+CREATE TABLE Users (
+    user_id INT IDENTITY(1,1) PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    CONSTRAINT chk_role CHECK (role IN ('Admin', 'Manager', 'Staff'))
+);
+
+CREATE TABLE Products (
+    product_id INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    stock_level INT DEFAULT 0
+);
+
+CREATE TABLE Suppliers (
+    supplier_id INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    contact_info VARCHAR(255) NOT NULL
+);
+
+
+
+
